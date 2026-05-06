f618c79f24c7cb6ca7356c98b15a8f39a8ff246c
TrySeriesLab <tryserieslab@gmail.com>
2026-05-06 09:27:50 -0400

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,9 +1,6 @@
 import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 export default async function handler(req, res) {
-const { Resend } = require('resend');
-const resend = new Resend(process.env.RESEND_API_KEY);
-module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
