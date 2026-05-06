c0c345ee992be9eb2a18f37073fad7646c13adf7
TrySeriesLab <tryserieslab@gmail.com>
2026-05-06 09:19:42 -0400

    Update webhook.js
    fix: ES module imports for resend

--- api/webhook.js
+++ api/webhook.js
@@ -1,3 +1,6 @@
+import { Resend } from 'resend';
+const resend = new Resend(process.env.RESEND_API_KEY);
+export default async function handler(req, res) {
 const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
 module.exports = async function handler(req, res) {
